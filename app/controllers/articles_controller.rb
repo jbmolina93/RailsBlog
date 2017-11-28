9c1b60dbe8970924f3f8f88c10c7c4c3f8604aa8
jbmol <jbmol@DESKTOP-043022S>
2017-11-27 19:03:34 -0800

    Security and authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,7 @@ class ArticlesController < ApplicationController
 	  end
 	end
 
+	http_basic_authenticate_with name: "Jonathan", password: "password", except: [:index, :show]
 	def index
 		@articles = Article.all
 	end
